f60ca13c8c3cedd335b9de7a2ffea713ea385384
François-Xavier Thomas <fx.thomas@gmail.com>
2013-02-22 13:58:00 +0100

    Initial Commit

---
+++ PocoFoundation/PocoFoundation.h
@@ -0,0 +1,13 @@
+//
+//  PocoFoundation.h
+//  PocoFoundation
+//
+//  Created by François-Xavier on 2/22/13.
+//  Copyright (c) 2013 Poco Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PocoFoundation : NSObject
+
+@end
---
+++ PocoFoundation/PocoFoundation.m
@@ -0,0 +1,13 @@
+//
+//  PocoFoundation.m
+//  PocoFoundation
+//
+//  Created by François-Xavier on 2/22/13.
+//  Copyright (c) 2013 Poco Project. All rights reserved.
+//
+
+#import "PocoFoundation.h"
+
+@implementation PocoFoundation
+
+@end
