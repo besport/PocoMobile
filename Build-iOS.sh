3e71e63e4878f1962eff65db9afd467d30214393
François-Xavier Thomas <fx.thomas@gmail.com>
2013-02-22 19:12:12 +0100

    Added source Git repo

---
+++ Build-iOS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: François-Xavier Thomas <fx.thomas@gmail.com>
+# Author: Be Sport 2012
+
+# Update Poco
+git submodule update
+
+# Build Poco
+cd Source/
+
+# Build iPhone libraries
+./configure --config=iPhone --no-tests --no-samples --omit=...
+make IPHONE_SDK_VERSION_MIN=3.0 POCO_TARGET_OSARCH=armv6 -s -j2
+make IPHONE_SDK_VERSION_MIN=3.2 POCO_TARGET_OSARCH=armv7 -s -j2
+
+# Build iPhone simulator libraries
+./configure --config=iPhoneSimulator --no-tests --no-samples --omit=...
+make -s -j2
