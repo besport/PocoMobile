42c3a99412adbfd13098aaae21ed4b24e7e5699c
François-Xavier Thomas <fx.thomas@gmail.com>
2013-03-29 17:07:36 +0100

    Build for armv7s

--- Build-iOS.sh
+++ Build-iOS.sh
@@ -13,6 +13,7 @@ cd Source/
 ./configure --config=iPhone --no-tests --no-samples --omit=...
 make IPHONE_SDK_VERSION_MIN=3.0 POCO_TARGET_OSARCH=armv6 -s -j2
 make IPHONE_SDK_VERSION_MIN=3.2 POCO_TARGET_OSARCH=armv7 -s -j2
+make IPHONE_SDK_VERSION_MIN=3.2 POCO_TARGET_OSARCH=armv7s -s -j2
 
 # Build iPhone simulator libraries
 ./configure --config=iPhoneSimulator --no-tests --no-samples --omit=...
